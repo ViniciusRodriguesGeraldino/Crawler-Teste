215dbf88cc0b2458bef5d2f55c38d69dc055c4da
vi.ni.geraldino@gmail.com <Vinicius>
2022-05-31 10:37:28 -0300

    .

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-git pull origin main &&
+git pull origin main  --allow-unrelated-histories  &&
 php bin/console cache:clear --no-warmup --env=prod &&
 php bin/console messenger:stop-workers
